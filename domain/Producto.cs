2e1dfd928c30a8e5fc6e19c302bb13248d121756
gonzalolanfranchi <gonzalanfranchi@gmail.com>
2023-04-20 12:00:15 -0300

    Acceso a datos

--- domain/Producto.cs
+++ domain/Producto.cs
@@ -10,7 +10,7 @@ namespace domain
     {
         public int Id { get; set; }
         public int Codigo { get; set; }
-        public string? Nombre { get; set; }
+        public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
---
+++ services/DataAccess.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace services
+{
+    public class DataAccess
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+
+        public DataAccess()
+        {
+            connection = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            command = new SqlCommand();
+        }
+
+        public void SetQuery(string query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = query;
+        }
+
+        public void ExecuteRead()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ExecuteAction()
+        {
+            command.Connection = connection;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
